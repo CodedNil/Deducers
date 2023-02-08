7e3c9edb6d84eb7bfafc67b1d5470d8f7fa7205c
nikolairaahauge <nira36696@edu.ucl.dk>
2023-02-08 21:05:12 +0100

    Adding simple functionality to input field

--- src/match.js
+++ src/match.js
@@ -1,6 +1,20 @@
+import { useState } from 'react';
 import './assets/scss/match.scss';
 
 function Match () {
+    
+    const [guess,setGuess] = useState('')
+
+    function sendGuess () {
+        setGuess('')
+    }
+
+    function handleKeyPress (key) {
+        if (key === 'Enter') {
+            sendGuess()
+        } 
+    }
+
     return (
         <main className="match">
             <aside className="match__side match__side--left">
@@ -10,8 +24,8 @@ function Match () {
 
             </aside>
             <div className='match__input-holder'>
-                <input type="text" className='input-holder__input' placeholder='Enter your question' />
-                <i className='holder__icon material-symbols-rounded'>east</i>
+                <input type="text" className='input-holder__input' placeholder='Enter your question' value={guess} onChange={e => setGuess(e.target.value)} onKeyDown={e => handleKeyPress(e.code)} />
+                <button onClick={() => sendGuess()} className='holder__icon material-symbols-rounded'>east</button>
             </div>
         </main>
     );
